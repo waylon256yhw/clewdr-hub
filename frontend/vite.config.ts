f7836b978995ebc0f76dbdc73fcea7365770b604
root <root@v2202511314978405645.supersrv.de>
2026-04-18 09:55:44 +0200

    build: split frontend vendor chunks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,29 @@ export default defineConfig({
   build: {
     outDir: '../static',
     emptyOutDir: true,
+    rolldownOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.includes('/node_modules/')) return undefined
+          if (/\/node_modules\/react(?:-dom)?\//.test(id) || /\/node_modules\/react-router\//.test(id)) {
+            return 'framework'
+          }
+          if (id.includes('/node_modules/@mantine/')) {
+            return 'mantine'
+          }
+          if (id.includes('/node_modules/@tanstack/react-query/')) {
+            return 'query'
+          }
+          if (id.includes('/node_modules/recharts/')) {
+            return 'charts'
+          }
+          if (id.includes('/node_modules/@tabler/icons-react/')) {
+            return 'icons'
+          }
+          return 'vendor'
+        },
+      },
+    },
   },
   server: {
     proxy: {
